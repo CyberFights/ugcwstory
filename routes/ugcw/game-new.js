21a712f8acafe215b75c5ca6e2fa7e50c0a7d95c
CyberFights <shadowjer011@gmail.com>
2026-09-12 16:04:03 -0400

    Update game-new.js

--- routes/ugcw/game-new.js
+++ routes/ugcw/game-new.js
@@ -96,7 +96,7 @@ $setVar[$getQuery[userid]-fishingrod;0]
 $setVar[terrain-ruincity1;x/x/x/x/x/ruincity2/x/x/x/x/x/-/x/x/x/x/x/-/ruincity3/-/-/-/-/-/x/x/-/x/x/x/x/x/-/x/x/x]
 $setVar[terrain-ruincity3;water/x/x/x/-/x/water/x/x/x/-/x/water/x/x/x/-/x/water/x/-/-/-/ruincity2/water/x/x/x/x/x/water/water/water/water/x/x]
 $setVar[terrain-ruincity2;x/ruincity4/-/-/-/-/x/-/x/x/x/-/x/x/x/x/x/-/x/x/x/x/x/-/x/x/x/x/x/-/x/x/x/x/x/ruincity1]
-$setVar[terrain-forest1;x/x/forest4/x/x/x/route1/-/-/grass/x/x/x/x/-/grass/x/x/x/grass/-/-/-/forest2/x/x/grass/grass/grass/x/x/x/x/x/x/x]
+$setVar[terrain-ruincity5;water/x/x/x/x/x/water/x/x/x/-/ruincity4/water/x/x/x/-/x/water/x/x/x/-/x/water/x/x/x/-/x/water/x/x/x/ruincity3/x]
 
   `
 }
