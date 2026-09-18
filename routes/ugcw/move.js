7250215bd6015ae579f0ab736140015ba9939974
CyberFights <shadowjer011@gmail.com>
2026-09-18 12:32:46 -0400

    Update move.js

--- routes/ugcw/move.js
+++ routes/ugcw/move.js
@@ -13,7 +13,7 @@ module.exports = {
   "stage": "$getVar[$getQuery[userid]-stage]",
   "current_map": "$get[map]",
   "encountered": "$getVar[$getQuery[userid]-encounter]",
-  "battle_image": "$ternary[$getVar[$getQuery[userid]-encounter]==true;https://shadows-api-pokemon.shadowsnemesis.repl.co/game/pokemon/encountered?userid=$getQuery[userid]&opponent=$getVar[$getQuery[userid]-opponent];null]",
+  "battle_image": "$ternary[$getVar[$getQuery[userid]-encounter]==true;https://github.com/CyberFights/ugcwstory/blob/main/assets/images/@getVar(@getQuery(userid)-location)-@getVar(@getQuery(userid)-opponent).jpeg;null]",
   "opponent": "$getVar[$getQuery[userid]-opponent]"
   }]
   $var[map;http://ugcwrp-production.up.railway.app/ugcw/maplocation?userid=$getQuery[userid]&steps=$getVar[$getQuery[userid]-steps]&position=$getVar[$getQuery[userid]-position]&location=$getVar[$getQuery[userid]-location]&time=$getQuery[time]&clock=$getQuery[clock]&stage=$getVar[$getQuery[userid]-stage]]
@@ -22,7 +22,8 @@ module.exports = {
   $setVar[$getQuery[userid]-steps;$getQuery[steps]]
 
   $setVar[$getQuery[userid]-time;$ternary[$getQuery[time]==AM;day;$ternary[$getQuery[time]==PM;night;]]]
-  
+    $tryIf[$getVar[$getQuery[userid]-opponent]==lizard;@setVar(@getQuery(userid)-oh;183) @setVar(@getQuery(userid)-ow;75)]
+  $tryIf[$getVar[$getQuery[userid]-opponent]==bandit;@setVar(@getQuery(userid)-oh;177) @setVar(@getQuery(userid)-ow;79)]
   $tryIf[$getVar[$getQuery[userid]-chance]==true;@setVar(@getQuery(userid)-encountered;true)]
   $setVar[$getQuery[userid]-chance;$random[true;false;false;false;false]]
   $tryif[$getVar[$getQuery[userid]-location]==ruincity1;@setVar(@getQuery(userid)-opponent;@get(random))
